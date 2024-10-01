3be66e5ee5ac58a4cc37b5f114b80d840a7201e0
Clarice <claricealves330@gmail.com>
2024-10-01 17:38:04 -0300

    ✨ feat: Adiciona Service de User

---
+++ src/api/services/UserServices.ts
@@ -0,0 +1,16 @@
+import { Repository } from 'typeorm';
+import connect from '../../database/connection';
+import { User } from '../../database/entities/User';
+
+export class UserServices {
+  private userRepository!: Repository<User>;
+
+  constructor() {
+    this.initializeRepository();
+  }
+
+  private async initializeRepository() {
+    const connection = await connect();
+    this.userRepository = connection.getRepository(User);
+  }
+}
