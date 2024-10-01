3c1857383f3d5cc7e7cc7325b175e9d7b4ddd6c3
Clarice <claricealves330@gmail.com>
2024-10-01 17:37:25 -0300

    ✨ feat: Adiciona Service de Acessory

---
+++ src/api/services/AcessoryServices.ts
@@ -0,0 +1,16 @@
+import { Repository } from 'typeorm';
+import connect from '../../database/connection';
+import { Acessory } from '../../database/entities/Acessory';
+
+export class AcessoryServices {
+  private acessoryRepository!: Repository<Acessory>;
+
+  constructor() {
+    this.initializeRepository();
+  }
+
+  private async initializeRepository() {
+    const connection = await connect();
+    this.acessoryRepository = connection.getRepository(Acessory);
+  }
+}
