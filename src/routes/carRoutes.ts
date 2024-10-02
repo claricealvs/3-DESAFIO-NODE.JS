b555b674d76f48520c1019d9ab666f091fed42c0
Clarice <claricealves330@gmail.com>
2024-10-01 22:30:17 -0300

    ✨ feat: Adiciona rota get de Cars

--- src/routes/carRoutes.ts
+++ src/routes/carRoutes.ts
@@ -5,6 +5,7 @@ import { CarController } from '../api/controllers/CarController';
 const router = Router();
 const carController = new CarController();
 
+router.get('/car', carController.getAllCars.bind(carController));
 router.post('/car', carController.createCar.bind(carController));
 
 export default router;
