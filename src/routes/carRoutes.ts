8404b5f3d11c86f6ccc7a5edd457628ed4c9be32
Clarice <claricealves330@gmail.com>
2024-10-02 14:53:27 -0300

    ✨ feat: Adiciona rota getCarById

--- src/routes/carRoutes.ts
+++ src/routes/carRoutes.ts
@@ -5,6 +5,7 @@ import { CarController } from '../api/controllers/CarController';
 const router = Router();
 const carController = new CarController();
 
+router.get('/car/:id', carController.getCarById.bind(carController));
 router.get('/car', carController.getAllCars.bind(carController));
 router.post('/car', carController.createCar.bind(carController));
 
