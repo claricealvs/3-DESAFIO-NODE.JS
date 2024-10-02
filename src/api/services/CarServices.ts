fe4a8b5310a7e8921ab08da406c5095e79da0073
Clarice <claricealves330@gmail.com>
2024-10-01 21:37:10 -0300

    ✨ feat: Adiciona método createCar

--- src/api/services/CarServices.ts
+++ src/api/services/CarServices.ts
@@ -1,8 +1,9 @@
 import { Repository } from 'typeorm';
 import connect from '../../database/connection';
 import { Car } from '../../database/entities/Car';
+import { Acessory } from '../../database/entities/Acessory';
 
-export class CarServices {
+export class CarService {
   private carRepository!: Repository<Car>;
 
   constructor() {
@@ -12,5 +13,28 @@ export class CarServices {
   private async initializeRepository() {
     const connection = await connect();
     this.carRepository = connection.getRepository(Car);
+  }
+
+  async createCar(
+    model: string,
+    color: string,
+    year: number,
+    valuePerDay: number,
+    acessories: Acessory[],
+    numberOfPassengers: number,
+  ): Promise<Car> {
+    const newCar = this.carRepository.create({
+      model,
+      color,
+      year,
+      valuePerDay,
+      acessories,
+      numberOfPassengers,
+    });
+
+    // Salvar no banco de dados
+    await this.carRepository.save(newCar);
+
+    return newCar;
   }
 }
