9b325f098a77a3378494fa08d8cc4a833698d4d1
Clarice <claricealves330@gmail.com>
2024-10-02 21:14:55 -0300

    ✨ feat: Adiciona método deleteCar

--- src/api/controllers/CarController.ts
+++ src/api/controllers/CarController.ts
@@ -179,4 +179,38 @@ export class CarController {
       }
     }
   }
+
+  async deleteCar(req: Request, res: Response) {
+    try {
+      const id = req.params.id;
+
+      await this.carService.deleteCar(id);
+
+      return res.status(204).json();
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        const code = (error as any).status || 400;
+
+        let statusMessage = '';
+
+        if (code == 400) {
+          statusMessage = 'Bad Request';
+        }
+
+        if (code == 404) {
+          statusMessage = 'Not Found';
+        }
+
+        return res.status(code).json({
+          code: code,
+          status: statusMessage,
+          message: error.message,
+        });
+      } else {
+        return res
+          .status(500)
+          .json({ error: 'An unexpected error has occurred.' });
+      }
+    }
+  }
 }
