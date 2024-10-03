ec17f095ae19793697fa0323fcc1228c8df9e018
Clarice <claricealves330@gmail.com>
2024-10-03 20:12:08 -0300

    ✨ feat: Adiciona rota put para updateUser

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -8,5 +8,6 @@ const userController = new UserController();
 router.post('/user', userController.createUser.bind(userController));
 router.get('/user', userController.getAllusers.bind(userController));
 router.get('/user/:id', userController.getUserById.bind(userController));
+router.put('/user/:id', userController.updateUser.bind(userController));
 
 export default router;
