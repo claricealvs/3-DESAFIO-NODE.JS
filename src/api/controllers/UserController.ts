f7a575475253341912b1fd5436edaae870ace081
Clarice <claricealves330@gmail.com>
2024-10-03 20:11:43 -0300

    ✨ feat: Adiciona método updateUser

--- src/api/controllers/UserController.ts
+++ src/api/controllers/UserController.ts
@@ -144,4 +144,74 @@ export class UserController {
       }
     }
   }
+
+  async updateUser(req: Request, res: Response) {
+    try {
+      const { name, cpf, birth, cep, email, password } = req.body;
+
+      const id = req.params.id;
+
+      if (!name || !cpf || !birth || !cep || !email || !password) {
+        return res.status(400).json({
+          code: 400,
+          status: 'Bad Request',
+          message: 'All fields are required.',
+        });
+      }
+
+      const birthParts = birth.split('/');
+      if (
+        birthParts.length !== 3 ||
+        isNaN(Date.parse(`${birthParts[0]}-${birthParts[1]}-${birthParts[2]}`))
+      ) {
+        return res.status(400).json({ error: 'Invalid birth date format.' });
+      }
+
+      const updatedUser = await this.userService.updateUser(
+        parseInt(id, 10),
+        name,
+        cpf,
+        birth,
+        cep,
+        email,
+        password,
+      );
+
+      const formattedUser = {
+        id: updatedUser.id,
+        name: updatedUser.name,
+        cpf: updatedUser.cpf,
+        birth: updatedUser.birth,
+        cep: updatedUser.cep,
+        email: updatedUser.email,
+        password: updatedUser.password,
+      };
+
+      return res.status(201).json(formattedUser);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        const code = (error as any).status || 400;
+
+        let statusMessage = '';
+
+        if (code == 400) {
+          statusMessage = 'Bad Request';
+        }
+
+        if (code == 404) {
+          statusMessage = 'Not Found';
+        }
+
+        return res.status(code).json({
+          code: code,
+          status: statusMessage,
+          message: error.message,
+        });
+      } else {
+        return res
+          .status(500)
+          .json({ error: 'An unexpected error has occurred.' });
+      }
+    }
+  }
 }
