d31d90d92a4a1cd89fc76e744d35630edb517587
Clarice <claricealves330@gmail.com>
2024-10-02 21:15:34 -0300

    ✨ feat: Adiciona rota Delete para deleteCar

--- src/routes/carRoutes.ts
+++ src/routes/carRoutes.ts
@@ -6,6 +6,7 @@ const router = Router();
 const carController = new CarController();
 
 router.get('/car/:id', carController.getCarById.bind(carController));
+router.delete('/car/:id', carController.deleteCar.bind(carController));
 router.get('/car', carController.getAllCars.bind(carController));
 router.post('/car', carController.createCar.bind(carController));
 router.put('/car/:id', carController.updateCar.bind(carController));
