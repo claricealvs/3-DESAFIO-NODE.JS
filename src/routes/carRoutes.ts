552ebd0833ea4b5b58d4489eb9a4e32a427e6a28
Clarice <claricealves330@gmail.com>
2024-10-02 21:00:45 -0300

    ✨ feat: Adiciona rota put para updateCar

--- src/routes/carRoutes.ts
+++ src/routes/carRoutes.ts
@@ -8,5 +8,6 @@ const carController = new CarController();
 router.get('/car/:id', carController.getCarById.bind(carController));
 router.get('/car', carController.getAllCars.bind(carController));
 router.post('/car', carController.createCar.bind(carController));
+router.put('/car/:id', carController.updateCar.bind(carController));
 
 export default router;
