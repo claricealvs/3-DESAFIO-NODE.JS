52a9a7ba8342ac910114c57c8f9196480f7f97dc
Clarice <claricealves330@gmail.com>
2024-10-03 19:38:45 -0300

    ✨ feat: Adiciona rota get para getAllUsers

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,5 +6,6 @@ const router = Router();
 const userController = new UserController();
 
 router.post('/user', userController.createUser.bind(userController));
+router.get('/user', userController.getAllusers.bind(userController));
 
 export default router;
