6fce09a2aeb07a81c274cc62d4b99f3c048602c3
Clarice <claricealves330@gmail.com>
2024-10-03 19:16:31 -0300

    ✨ feat: Adiciona método createUser

--- src/api/services/UserServices.ts
+++ src/api/services/UserServices.ts
@@ -13,4 +13,53 @@ export class UserService {
     const connection = await connect();
     this.userRepository = connection.getRepository(User);
   }
+
+  async createUser(
+    name: string,
+    cpf: string,
+    birth: string,
+    cep: string,
+    email: string,
+    password: string,
+  ): Promise<User> {
+    if (
+      ![name, cpf, cep, email, password].every(
+        (value) => typeof value === 'string',
+      )
+    ) {
+      throw new Error('Incompatible data value.');
+    }
+
+    const [day, month, year] = birth.split('/');
+    const formattedBirth = new Date(
+      Number(year),
+      Number(month) - 1,
+      Number(day),
+    );
+
+    // Verificar se a data é válida
+    if (isNaN(formattedBirth.getTime())) {
+      throw new Error('Invalid birth date format');
+    }
+
+    const newUser = this.userRepository.create({
+      name,
+      cpf,
+      birth: formattedBirth,
+      cep,
+      email,
+      password,
+    });
+
+    await this.userRepository.save(newUser);
+
+    return newUser;
+  }
+
+  formatDateToBrazilian(date: Date): string {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
 }
