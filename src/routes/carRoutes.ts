8db7d233126ee40368c01e0595ddaa5fb23ea367
Clarice <claricealves330@gmail.com>
2024-10-02 22:19:28 -0300

    ✨ feat: Adiciona rota patch para updateCarAccesories

--- src/routes/carRoutes.ts
+++ src/routes/carRoutes.ts
@@ -6,6 +6,10 @@ const router = Router();
 const carController = new CarController();
 
 router.get('/car/:id', carController.getCarById.bind(carController));
+router.patch(
+  '/car/:id',
+  carController.updateCarAccessories.bind(carController),
+);
 router.delete('/car/:id', carController.deleteCar.bind(carController));
 router.get('/car', carController.getAllCars.bind(carController));
 router.post('/car', carController.createCar.bind(carController));
