5f9a61ac82a1f367c18ee913bc432373291cb060
Clarice <claricealves330@gmail.com>
2024-10-03 19:17:45 -0300

    ✨ feat: Adiciona userRoutes com rota post para createUser

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+import { UserController } from '../api/controllers/UserController';
+
+const router = Router();
+const userController = new UserController();
+
+router.post('/user', userController.createUser.bind(userController));
+
+export default router;
