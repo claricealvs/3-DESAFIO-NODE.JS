3d913a5011abf169a73bf541cfd80605b1ff6261
Clarice <claricealves330@gmail.com>
2024-10-03 19:18:19 -0300

    ✨ feat: Adiciona rotas de User

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connect from './database/connection';
 import carRoutes from './routes/carRoutes';
+import userRoutes from './routes/userRoutes';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.use('/v1', carRoutes);
+app.use('/v1', carRoutes, userRoutes);
 app.use('/', (req, res) => res.send('API funcionando!'));
 
 async function startServer() {
