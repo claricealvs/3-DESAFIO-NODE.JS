52f64f3ad37372a1ef66fb19eb2c028d0035a9a8
Clarice <claricealves330@gmail.com>
2024-10-03 22:22:20 -0300

    ✨ feat: Adiciona rota delete para deleteUser

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -9,5 +9,6 @@ router.post('/user', userController.createUser.bind(userController));
 router.get('/user', userController.getAllusers.bind(userController));
 router.get('/user/:id', userController.getUserById.bind(userController));
 router.put('/user/:id', userController.updateUser.bind(userController));
+router.delete('/user/:id', userController.deleteUser.bind(userController));
 
 export default router;
