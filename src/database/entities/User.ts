70e8634c18d1bc1c92e4129cd7b809487e4fbea4
Clarice <claricealves330@gmail.com>
2024-10-08 14:34:13 -0300

    ✨ feat: Adiciona relacionamento com Reserve

--- src/database/entities/User.ts
+++ src/database/entities/User.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Reserve } from './Reserve';
 
 @Entity('users')
 export class User {
@@ -22,4 +23,7 @@ export class User {
 
   @Column()
   password!: string;
+
+  @OneToMany(() => Reserve, (reserve) => reserve.user)
+  reserves!: Reserve[];
 }
